eee82119161f192176393e18c752ec329e7d06a3
GuillaumeMaugin <91453633+GuillaumeMaugin@users.noreply.github.com>
2021-10-05 19:50:31 +0200

    Update streambox.js

--- streambox.js
+++ streambox.js
@@ -1,20 +1,35 @@
 const fs = require('fs')
 const http = require('http')
 const { Transform } = require('stream')
+const TransformStream = new Transform();
 //Import des fonctions fs et http
 
 function duplicate(filename){
+    const [name, extension] = ('.')
     const s =fs.createReadStream(filename)
     //créer la cont s qui créer le flux de sortie depuis le filename
-    const d =fs.createWriteStream('./duplicate.txt')
+    const d =fs.createWriteStream(name + '-copie' + extension)
     //créer la const d qui créer le flux d'entrée vers duplicate.txt
     s.pipe(d)
 // lie la sortie du flux (s) à l'entrée du flux (d) (filename)[s]==>==[d](duplicate.txt)
 }
 
-function transform(filename, re, fn){
-    const s1 =fs.createReadStream(filename)
 
+
+function transform(filename, re, fn){
+    TransformStream._transform = 
+    //change la fonction (._transform) en ma propre fonction
+    function(chunk, encoding, callback) {
+        TransformStream.push(chunk.toString().split(re).join(fn(re)))
+        //sépare en chunk le texte, coupe le 're' (.split(re)) et remplace pas le résultat de la fonction fn (.join(fn(re)))
+        callback();
+        //signifie qu'il a fini et attend les nouvelles données à traiter 
+    }
+
+    const s = fs.createReadStream(filename);
+    const p = fs.createWriteStream('log.txt')
+
+    s.pipe(TransformStream).pipe(p)
 }
 
 module.exports = {
